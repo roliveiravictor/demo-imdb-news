d8fc0679f19eeaae5fd3cefb5eade42343836217
Victor R <victor.rocha@mercadolivre.com>
2019-08-15 09:06:14 -0300

    fixed stand alone test from data source factory requiring context for core repository

--- app/src/androidTest/java/com/stonetree/imdbnews/feature/latest/res/factory/LatestDataSourceFactoryTest.kt
+++ app/src/androidTest/java/com/stonetree/imdbnews/feature/latest/res/factory/LatestDataSourceFactoryTest.kt
@@ -1,8 +1,14 @@
 package com.stonetree.imdbnews.feature.latest.res.factory
 
 import androidx.lifecycle.MutableLiveData
+import androidx.navigation.findNavController
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import com.stonetree.corerepository.feature.idling.CoreRepositoryIdling
+import com.stonetree.imdbnews.MainView
+import com.stonetree.imdbnews.R
 import com.stonetree.imdbnews.feature.latest.res.repository.LatestRepository
 import junit.framework.TestCase.assertNull
 import junit.framework.TestCase.assertNotNull
@@ -10,6 +16,7 @@ import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.any
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -18,12 +25,15 @@ class LatestDataSourceFactoryTest {
 
     private lateinit var factory: LatestDataSourceFactory
 
-    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+    private lateinit var repository: LatestRepository
 
-    private val repository = LatestRepository.getInstance()
+    @Rule
+    @JvmField
+    val rule = ActivityTestRule(MainView::class.java)
 
     @Before
     fun setup() {
+        repository = LatestRepository.getInstance()
         factory = LatestDataSourceFactory(repository)
     }
 
