2acf764c2374251b055443fbc4df1cf53a53a28e
Victor R <victor.rocha@mercadolivre.com>
2019-08-16 10:01:11 -0300

    Added details model test

---
+++ app/src/test/java/com/stonetree/imdbnews/feature/details/model/DetailsModelTest.kt
@@ -0,0 +1,16 @@
+package com.stonetree.imdbnews.feature.details.model
+
+import junit.framework.TestCase.*
+import org.junit.Test
+
+class DetailsModelTest {
+
+    @Test
+    fun test_detailsModel_shouldReturnDefaultValues() {
+        DetailsModel().apply {
+            assertNull(title)
+            assertNull(description)
+            assertNull(poster)
+        }
+    }
+}
