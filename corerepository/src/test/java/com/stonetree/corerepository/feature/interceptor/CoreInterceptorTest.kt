774e822186af757db34a95aba83724474a5c279c
Victor R <victor.rocha@mercadolivre.com>
2019-10-15 19:38:46 -0300

    modified interceptor test

--- corerepository/src/test/java/com/stonetree/corerepository/feature/interceptor/CoreInterceptorTest.kt
+++ corerepository/src/test/java/com/stonetree/corerepository/feature/interceptor/CoreInterceptorTest.kt
@@ -4,12 +4,30 @@ import junit.framework.TestCase.assertEquals
 import okhttp3.logging.HttpLoggingInterceptor.Level.BODY
 import org.junit.Before
 import org.junit.Test
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.inject
 
-class CoreInterceptorTest {
+class CoreInterceptorTest: AutoCloseKoinTest() {
+
+    private val module = module {
+        factory<CoreInterceptor>{ CoreInterceptorImpl() }
+    }
+
+    private val interceptor: CoreInterceptor by inject()
+
+    @Before
+    fun setup() {
+        startKoin {
+            loadKoinModules(module)
+        }
+    }
 
     @Test
     fun test_logging_shouldReturnLevelBody() {
-        val logging = CoreInterceptorImpl().log()
+        val logging = interceptor.log()
         assertEquals(BODY, logging.level)
     }
 }
